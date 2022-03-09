482ec17203fcc37a797e3f00caa33b084fc9059d
marco vavassori <56401413+MarcoVavassori90@users.noreply.github.com>
2022-03-09 17:01:51 +0000

    Somma Lista soluzione 1

--- primo_ese.py
+++ primo_ese.py
@@ -1,6 +1,7 @@
-my_list = [1,3,5,2,4]
 
-print ("The sum of my_list is", sum(my_list))
+
+
+
 
 def sum_of_list(l):
   total = 0
@@ -8,4 +9,6 @@ def sum_of_list(l):
     total = total + val
   return total
 
+my_list = [1,3,5,2,4]
 print ("The sum of my_list is", sum_of_list(my_list))
+
---
+++ primo_ese1.py
@@ -0,0 +1,3 @@
+
+my_list = [1,3,5,2,4]
+print ('The sum of my_list is', sum(my_list))
